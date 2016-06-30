fe8ea5ba41fed71c5e127908ab731bd438f1c6b7
İlayda Yorulmaz <ilaydaayorulmaz@gmail.com>
2016-06-30 16:38:33 +0300

    District class

---
+++ src/main/java/ob/geocoding/District.java
@@ -0,0 +1,38 @@
+package ob.geocoding;
+
+public class District {
+
+	private int districtId;
+	private int townId;
+	private String districtName;
+	
+	public District(){
+		
+	}
+
+	public int getDistrictId() {
+		return districtId;
+	}
+
+	public void setDistrictId(int districtId) {
+		this.districtId = districtId;
+	}
+
+	public int getTownId() {
+		return townId;
+	}
+
+	public void setTownId(int townId) {
+		this.townId = townId;
+	}
+
+	public String getDistrictName() {
+		return districtName;
+	}
+
+	public void setDistrictName(String districtName) {
+		this.districtName = districtName;
+	}
+
+	
+}
