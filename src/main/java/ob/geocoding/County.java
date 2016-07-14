d3ac4646af2dc14cacb4f64ba0bc5c9f3b3f9b2c
İlayda Yorulmaz <ilaydaayorulmaz@gmail.com>
2016-07-14 08:47:50 +0300

    County class added.

---
+++ src/main/java/ob/geocoding/County.java
@@ -0,0 +1,41 @@
+package ob.geocoding;
+
+public class County {
+
+	public County(int countyId, String countyName) {
+		super();
+		this.countyId = countyId;
+		this.countyName = countyName;
+	}
+	private int countyId;
+	private String countyName;
+	private int cityId;
+    public int getCountyId() {
+		return countyId;
+	}
+    @Override
+	public String toString() {
+		return String.valueOf(countyId) + " " + countyName ;
+	}
+
+	public void setTownId(int townId) {
+		this.countyId = townId;
+	}
+
+	public String getTownName() {
+		return countyName;
+	}
+
+	public void setTownName(String townName) {
+		this.countyName = countyName;
+	}
+
+	public int getCityId() {
+		return cityId;
+	}
+
+	public void setCityId(int cityId) {
+		this.cityId = cityId;
+	}
+
+}
